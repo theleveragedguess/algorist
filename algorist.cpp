701682598a362965f0ae677a13fb2b01ed0e76c6
Saad Khoudmi <khoudmi@gmail.com>
2018-10-20 14:03:36 +0100

    cpp code cleanup

--- algorist.cpp
+++ algorist.cpp
@@ -1,19 +1,9 @@
 #include <iostream>
-#include "part_1/chapter_1/programs.h"
 
 using namespace std;
 
 int main()
 {
 	cout << "The Algorist" << endl;
-	cout << "# Part One" << endl;
-
-	{		
-		cout << "## Chapter One" << endl
-		<< "### The Lotto Psychic" << endl;
-		part_one_chapter_one::lottoPsychic({ 15u, 6u, 6u, 4u });
-	}
-		
-
 	return 0;
 }
