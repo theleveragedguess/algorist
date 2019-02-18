a1f1709d3c754a8a870543c8c4875d5a4b50f7d8
Saad Khoudmi <khoudmi@gmail.com>
2019-02-18 15:57:55 +0100

    editorconfig update

--- common/combinatorics.h
+++ common/combinatorics.h
@@ -2,18 +2,18 @@
 
 namespace combinatorics
 {
-    typedef unsigned int uint;
+	typedef unsigned int uint;
 
-    uint factorial(const uint upper, const uint lower = 1);
+	uint factorial(const uint upper, const uint lower = 1);
 
-    uint subsetCount(const uint n, const uint r);
+	uint subsetCount(const uint n, const uint r);
 
-    // This invertible, deterministic function produce an unique sequencing index for a subset r of a set n
-    // the index is between 0 and subsetCount(n,r)-1
-    // We assume the subset is given in its unique ascending ordering, otherwise the function will be wrong
-    uint linearizeSubset(const uint n, const uint r, const uint* subset);
+	// This invertible, deterministic function produce an unique sequencing index for a subset r of a set n
+	// the index is between 0 and subsetCount(n,r)-1
+	// We assume the subset is given in its unique ascending ordering, otherwise the function will be wrong
+	uint linearizeSubset(const uint n, const uint r, const uint *subset);
 
-    // This is the inverse function of linearizeSubset
-    // The outputted subset is in its unique ascending ordering
-    void unlinearizeSubset(const uint n, const uint r, const uint linearIndex, uint* subset);
-}
+	// This is the inverse function of linearizeSubset
+	// The outputted subset is in its unique ascending ordering
+void unlinearizeSubset(const uint n, const uint r, const uint linearIndex, uint *subset);
+} // namespace combinatorics
